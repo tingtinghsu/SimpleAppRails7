33fda71a16efccc0549099f3e0443a833803fa80
tingtinghsu <tingtinghsu.tw@gmail.com>
2022-06-11 21:02:57 +0800

    Try tailwind css and add an action that responds to a DOM event
    ref:
    https://www.simplethread.com/how-to-create-a-new-rails-7-app-with-tailwind/

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    this.element.textContent = "Hello Ting Ting! This is Rails 7 app :)"
+  }
+  handleClick() {
+    this.element.textContent = "I am clicked."
   }
 }
