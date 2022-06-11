70644782789e278dd94caa36340fa34374584f27
tingtinghsu <tingtinghsu.tw@gmail.com>
2022-06-11 22:51:38 +0800

    Use rails g administrate:install to add dashboard for Author/Post model
    Also add authors#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :admin do
+      resources :authors
+      resources :posts
+
+      root to: "authors#index"
+    end
   root 'public#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
